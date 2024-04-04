c19d552666ba440c81e2ca58536656a397fe095d
user <user@buhfurlaptop.buhfurnet.com>
2024-04-04 18:40:59 -0400

    in the install_go.sh script, the script now requires to be run as super user

--- install_scripts/install_go.sh
+++ install_scripts/install_go.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.2.linux-amd64.tar.gz 
-wait
+
+is_sudo = $SUDO_USER
+
+if [ -z $is_sudo ]; then
+        echo "Script is not running as super user! " 
+        echo "Please run this script as superuser ! "
+        exit
+fi
+
+
 # download golang binaries 
 wget https://go.dev/dl/go1.22.2.linux-amd64.tar.gz -O /home/$SUDO_USER/Downloads
 wait
