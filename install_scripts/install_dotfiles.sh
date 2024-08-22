9e5e4031fde1dbdd28de92e60bb8a5bc172a402d
buhfur <ryanmcvicker@protonmail.com>
2024-08-22 17:05:14 -0400

    updated dotfiles

--- install_scripts/install_dotfiles.sh
+++ install_scripts/install_dotfiles.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-if  [ $SHELL = /usr/bin/zsh ]; then
-    cat ../dotfiles/zshrc > ~/.zshrc 
-fi
-
+cat ../dotfiles/zshrc > ~/.zshrc 
 cat ../dotfiles/bashrc > ~/.bashrc 
 cat ../dotfiles/tmux.conf > ~/.tmux.conf
 cat ../dotfiles/vimrc > ~/.vimrc
