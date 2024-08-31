8609b5c098f16bee7d35615a96dc87fd32b6a368
root <root@RMcVicker-Dell-Lat7520>
2024-08-31 18:16:54 -0400

    added docker adguard script

---
+++ install_scripts/docker/adguard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker run --name adguardhome\
+    --restart unless-stopped\
+    -v /my/own/workdir:/opt/adguardhome/work\
+    -v /my/own/confdir:/opt/adguardhome/conf\
+    -p 53:53/tcp -p 53:53/udp\
+    -p 67:67/udp -p 68:68/udp\
+    -p 80:80/tcp -p 443:443/tcp -p 443:443/udp -p 3000:3000/tcp\
+    -p 853:853/tcp\
+    -p 853:853/udp\
+    -p 5443:5443/tcp -p 5443:5443/udp\
+    -p 6060:6060/tcp\
+    -d adguard/adguardhome
