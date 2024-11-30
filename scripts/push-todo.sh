10d2a748eb548379a7dae28f3973b5455ce9f390
root <root@RMcVicker-Dell-Lat7520>
2024-11-30 15:32:10 -0500

    Auto-commit: Sat Nov 30 15:32:10 EST 2024

---
+++ scripts/push-todo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# bash script which commits changes made on the old-wiki repo 
+
+
+REPO_PATH="/root/todo/"
+cd $REPO_PATH || exit
+
+# Check for changes
+if [ -n "$(git status --porcelain)" ]; then
+    # Stage all changes
+    git add -A
+    # Commit with a timestamp
+    git commit -m "Auto-commit: $(date)"
+    # (Optional) Push to remote
+    git push origin master  # Replace 'main' with your branch name if needed
+fi
